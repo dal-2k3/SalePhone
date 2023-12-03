75d42344a1e9592c40c3d0aab5927b1b8d0208f2
worklist <baodan307@gmail.com>
2023-12-03 08:55:11 +0700

    contact news 404

---
+++ frontend/src/page/Contact.js
@@ -0,0 +1,76 @@
+import React, { Component } from 'react';
+
+class Contact extends Component {
+  render() {
+    return (
+      <div className="bg-gray-100 flex items-center justify-center pt-10 ">
+        <div className="w-4/5 mt-8">
+
+          <div className="mb-2 text-center">
+            <h1 style={{ fontSize: '24px' }}>Giới thiệu SalePhone</h1>
+          </div>
+          <div className="mb-2">
+            <h5 className="text-lg font-bold mb-2" style={{ fontSize: '16px' }}>
+               Giới thiệu chung
+            </h5>
+            <p className="text-xs " style={{ fontSize: '14px', lineHeight:'1.3rem'}}>
+            SalePhone được thành lập với sứ mệnh đem lại trải nghiệm mua sắm tuyệt vời nhất cho khách hàng trong lĩnh vực điện thoại di động. Với đội ngũ nhân viên chuyên nghiệp và nhiệt huyết, chúng tôi cam kết mang đến sự hài lòng và tin tưởng cho mỗi khách hàng.
+            Chúng tôi tự hào là đối tác của những thương hiệu hàng đầu trong ngành công nghiệp di động, cung cấp một bộ sưu tập đa dạng của các dòng sản phẩm mới nhất và tiên tiến nhất. SalePhone không chỉ là nơi để mua sắm điện thoại, mà còn là địa điểm lý tưởng để tìm kiếm các phụ kiện độc đáo và chất lượng.
+            Tại SalePhone, chúng tôi coi trọng việc tạo ra một môi trường mua sắm trực tuyến và offline thuận lợi và an tâm. Đội ngũ chăm sóc khách hàng của chúng tôi luôn sẵn sàng hỗ trợ bạn từ việc chọn lựa sản phẩm đến sau khi mua hàng, để đảm bảo mọi trải nghiệm của bạn là không thể nào quên.
+            Chất lượng và sự đáng tin cậy là tôn chỉ hàng đầu của SalePhone. Chúng tôi cam kết cung cấp sản phẩm chính hãng và dịch vụ xuất sắc, giúp bạn hoàn toàn yên tâm với mỗi giao dịch.
+            Hãy đến với SalePhone, nơi mà sự lựa chọn thông minh và thú vị bắt đầu. Cùng chúng tôi khám phá thế giới không ngừng đổi mới của công nghệ di động!
+            </p>
+          </div>
+          <div className="mb-2">
+            <h5 className="text-lg font-bold mb-2" style={{ fontSize: '16px' }}>
+               Sự Đa Dạng Đẳng Cấp - Chất Lượng Đỉnh Cao
+            </h5>
+            <p className="text-xs " style={{ fontSize: '14px' }}>
+              SalePhone là nơi bạn có thể tìm thấy sự đa dạng độc đáo của thế giới điện thoại di động, từ những chiếc smartphone tiên tiến đến những chiếc điện thoại giá trung bình với hiệu suất vô song. Chúng tôi chọn lựa cẩn thận từ những thương hiệu hàng đầu để đảm bảo rằng bạn chỉ nhận được sự đẳng cấp và chất lượng tốt nhất. SalePhone là điểm đến cho những người yêu thích sự đa dạng và độc đáo trong điện thoại di động. Chúng tôi liên tục cập nhật danh mục sản phẩm để đáp ứng đa dạng nhu cầu của khách hàng. Từ những chiếc smartphone cao cấp cho đến những mẫu giá trung bình, mỗi sản phẩm đều được chọn lựa kỹ lưỡng để đảm bảo sự đa dạng không ngừng.
+            </p>
+          </div>
+
+          <div className="mb-0.5">
+            <h5 className="text-lg font-bold mb-2" style={{ fontSize: '16px' }}>
+               Giá Trị Tốt Nhất - Ưu Đãi Không Ngừng
+            </h5>
+            <p className="text-xs" style={{ fontSize: '14px' }}>
+              Tại SalePhone, chúng tôi cam kết mang lại giá trị tốt nhất cho mỗi sản phẩm. Bạn sẽ không chỉ mua được điện thoại chính hãng với giá hợp lý mà còn được trải nghiệm những ưu đãi và khuyến mãi đặc biệt chỉ có tại cửa hàng của chúng tôi. Hãy đảm bảo theo dõi chúng tôi để không bỏ lỡ những cơ hội vàng!
+            </p>
+          </div>
+
+          <div className="mb-0.5">
+            <h5 className="text-lg font-bold mb-2" style={{ fontSize: '16px' }}>
+               Trải Nghiệm Mua Sắm An Toàn và Thuận Tiện
+            </h5>
+            <p className="text-xs" style={{ fontSize: '14px' }}>
+            Ở SalePhone, chúng tôi không chỉ tập trung vào việc mang đến những sản phẩm hàng đầu với công nghệ tiên tiến, mà còn chú trọng đến hiệu suất. Dù bạn là người theo đuổi công nghệ mới nhất hay đang tìm kiếm sự ổn định và tiện ích, chúng tôi cam kết mang đến trải nghiệm vượt trội mỗi khi sử dụng điện thoại của chúng tôi. Chúng tôi đặt sự an toàn và thuận tiện của bạn lên hàng đầu. Hệ thống thanh toán an toàn và bảo mật cao cấp của chúng tôi đảm bảo rằng mọi giao dịch của bạn đều được bảo vệ. Bạn có thể yên tâm mua sắm trực tuyến và chờ đợi sản phẩm yêu thích của mình được giao đến cửa nhà.
+            </p>
+          </div>
+
+          <div className="mb-0.5">
+            <h5 className="text-lg font-bold mb-2" style={{ fontSize: '16px' }}>
+               Đội Ngũ Chăm Sóc Khách Hàng Tận Tâm
+            </h5>
+            <p className="text-xs" style={{ fontSize: '14px' }}>
+              SalePhone không chỉ là nơi mua sắm, mà còn là nơi bạn có thể đặt niềm tin. Đội ngũ chăm sóc khách hàng của chúng tôi luôn sẵn sàng hỗ trợ bạn 24/7, từ việc tư vấn chọn mua đến giải quyết mọi vấn đề. Chúng tôi chắc chắn bạn sẽ có trải nghiệm mua sắm tuyệt vời nhất.
+            </p>
+          </div>
+
+          <div className="">
+            <h5 className="text-lg font-bold mb-2" style={{ fontSize: '16px' }}>
+               Liên Hệ Với Chúng Tôi:
+            </h5>
+            <p className="text-xs" style={{ fontSize: '14px' }}>
+              Địa chỉ cửa hàng: 137 Nguyễn Thị Thập<br />
+              Hotline: (+84) 123456789<br />
+              Email: salephone@gmail.com
+            </p>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Contact;
